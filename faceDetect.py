c5362f21fed034cd56691f59abd8f3b7f3b5778c
Karthik Subramanian <45620176+sktg84@users.noreply.github.com>
2023-05-02 07:09:15 +0530

    Update faceDetect.py

--- faceDetect.py
+++ faceDetect.py
@@ -1,11 +1,8 @@
-
 import cv2
 import pygame
 import numpy as np
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('/Users/ksubram2/Documents/Data/Work/Projects/python/sandbox/haarcascade_frontalface_default.xml')
 
 pygame.init()
 pygame.font.init()
@@ -17,6 +14,7 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 
 menu_items = [
+    {'label': 'Connect', 'action': lambda: start_camera(), 'rect': pygame.Rect(0, 0, 0, 0)},
     {'label': 'About', 'action': lambda: print('Designed by Karthik'), 'rect': pygame.Rect(0, 0, 0, 0)},
     {'label': 'Exit', 'action': lambda: exit(0), 'rect': pygame.Rect(0, 0, 0, 0)},
 ]
@@ -33,15 +31,13 @@ for item in menu_items:
 
 screen = pygame.display.set_mode((640, 480+menu_bar_rect.height))
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        continue
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+def start_camera():
+    global cap
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit(0)
@@ -57,11 +53,18 @@ while True:
     for item in menu_items:
         text = font.render(item['label'], True, white)
         screen.blit(text, item['rect'])
-    frame = np.rot90(frame, k=1)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame = pygame.surfarray.make_surface(frame)
-    screen.blit(frame, (0, 30))
+
+    if 'cap' in globals():
+        ret, frame = cap.read()
+        if ret:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            frame = np.rot90(frame, k=1)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame = pygame.surfarray.make_surface(frame)
+            screen.blit(frame, (0, 30))
 
     pygame.display.flip()
-
     clock.tick(60)
